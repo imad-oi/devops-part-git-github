bdaa9c2d3db04d409ca01410a91c231fb47dfec8
ENNASTY <ennastyhamza42@gmail.com>
2024-10-07 17:19:52 +0100

    Login

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { test } = require('./src/controllers/auth.controller');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { test } = require("./src/controllers/auth.controller");
+const { login } = require("./src/controllers/auth.controller");
 
 const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/ecommerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Middleware
 app.use(bodyParser.json());
 
 // Simple route
-app.get('/', (req, res) => {
-  res.send('Welcome to the E-commerce API');
+app.get("/", (req, res) => {
+  res.send("Welcome to the E-commerce API");
 });
 
-app.post('/auth/test',test)
+app.post("/auth/test", test);
+app.post("/auth/login", login);
 
 // Start the server
 app.listen(port, () => {
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,18 @@
 import { User } from "../models/User";
 
-export const test = async (req, res) => {
-}
+export const test = async (req, res) => {};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, password });
+    if (user) {
+      res.send("Login successful!");
+    } else {
+      res.status(401).send("Invalid username or password");
+    }
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+};
